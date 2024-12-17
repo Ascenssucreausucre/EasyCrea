cc2e7935ef5f5e95d7cf233e399c1e92949730e1
unknown <amiotflorian33@gmail.com>
2024-12-17 20:35:29 +0100

    pitié

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { NavBar } from "../components/Navbar";
+import { NavBar } from "../components/Navbar.jsx";
 import { Deck } from "../components/Deck";
 
 async function getDeck() {
