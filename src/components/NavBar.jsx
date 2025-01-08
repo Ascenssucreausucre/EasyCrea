4114e7348688bdacc337c218f39a74d3ebbb8467
unknown <amiotflorian33@gmail.com>
2025-01-08 02:24:58 +0100

    burger btn du bled

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,46 +1,79 @@
 import { Link, NavLink } from "react-router-dom";
 import { useUser } from "../context/UserContext";
+import { useState } from "react";
 import logo from "/src/assets/img/EasyCrea.svg";
+import burger from "../assets/img/menu.png";
 
 export function NavBar() {
   const { userData, logout } = useUser();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
     <div className="navbar">
       <Link to={"/"}>
-        <img src={logo} alt="" />
+        <img src={logo} alt="Logo EasyCrea" className="logo" />
       </Link>
 
-      <nav>
-        <NavLink to="/" className="nav-link">
+      {/* Bouton Burger */}
+      <div className="burger" onClick={toggleMenu}>
+        <img src={burger} alt="Menu Burger" />
+      </div>
+
+      {/* Menu */}
+      <nav className={`nav-menu ${menuOpen ? "active" : ""}`}>
+        <NavLink to="/" className="nav-link" onClick={() => setMenuOpen(false)}>
           Decks
         </NavLink>
 
         {userData ? (
           <>
             {userData.userType === "administrateur" ? (
-              <NavLink to={`/administrateur/`} className="nav-link">
+              <NavLink
+                to={`/administrateur/`}
+                className="nav-link"
+                onClick={() => setMenuOpen(false)}
+              >
                 Dashboard
               </NavLink>
             ) : (
               <NavLink
                 to={`/createur/${userData.id_createur}`}
                 className="nav-link"
+                onClick={() => setMenuOpen(false)}
               >
                 Profil
               </NavLink>
             )}
 
-            <NavLink to="/" onClick={logout} className="button logout-btn">
+            <NavLink
+              to="/"
+              onClick={() => {
+                logout();
+                setMenuOpen(false);
+              }}
+              className="button logout-btn"
+            >
               Déconnexion
             </NavLink>
           </>
         ) : (
           <>
-            <NavLink to="/login" className="button login-btn">
+            <NavLink
+              to="/login"
+              className="button login-btn"
+              onClick={() => setMenuOpen(false)}
+            >
               Login
             </NavLink>
-            <NavLink to="/sign-up" className="button sign-up-btn">
+            <NavLink
+              to="/sign-up"
+              className="button sign-up-btn"
+              onClick={() => setMenuOpen(false)}
+            >
               Sign-Up
             </NavLink>
           </>
